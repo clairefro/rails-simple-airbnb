93903da5e7281152bb4d93d2fcc641521afb0baf
claire froelich <claire.froelich@gmail.com>
2019-11-13 16:28:19 -0500

    flat validators and seed set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "deleting old data..."
+Flat.destroy_all
+
+puts "creating new data..."
+
+10.times do
+  flats_attributes = [
+    name: Faker::Commerce.product_name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description ,
+    price_per_night: rand(25..99),
+    number_of_guests: rand(1..4)
+  ]
+  Flat.create!(flats_attributes)
+end
+puts "done!"
